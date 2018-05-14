51df88cd0e10cb4b64b10cb41a0c338dc108fe48
yaskovdev <yaskovdev@gmail.com>
2018-05-14 21:27:11 +0300

    The message.

--- time-is-money-back-end.sh
+++ time-is-money-back-end.sh
@@ -5,14 +5,14 @@ export SPRING_DATA_MONGODB_PORT=27017
 
 SERVICE_NAME=time-is-money-back-end
 SERVICE_FOLDER=/app/time-is-money-backend
-PID_PATH_NAME=${SERVICE_FOLDER}/application.pid
+PID_PATH_NAME=${SERVICE_FOLDER}/target/application.pid
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ -f ${PID_PATH_NAME} ]; then
             echo "$SERVICE_NAME is already running ..."
         else
-            ${SERVICE_FOLDER}/mvnw spring-boot:run
+            java -jar /app/time-is-money-backend/target/timer-0.0.1-SNAPSHOT.jar
             echo "$SERVICE_NAME started ..."
         fi
     ;;
@@ -35,7 +35,7 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm ${PID_PATH_NAME}
             echo "$SERVICE_NAME starting ..."
-            ${SERVICE_FOLDER}/mvnw spring-boot:run
+            java -jar /app/time-is-money-backend/target/timer-0.0.1-SNAPSHOT.jar
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is not running ..."
