19be4387440ca3c612126afd293f3731f2bcdb00
yaskovdev <yaskovdev@gmail.com>
2018-05-15 20:17:41 +0300

    The message.

--- time-is-money-back-end.sh
+++ time-is-money-back-end.sh
@@ -8,37 +8,37 @@ SERVICE_FOLDER=/app/time-is-money-backend
 PID_PATH_NAME=${SERVICE_FOLDER}/target/application.pid
 case $1 in
     start)
-        echo "Starting $SERVICE_NAME ..."
+        echo "Starting $SERVICE_NAME..."
         if [ -f ${PID_PATH_NAME} ]; then
-            echo "$SERVICE_NAME is already running ..."
+            echo "$SERVICE_NAME is already running..."
         else
             nohup java -jar /app/time-is-money-backend/target/timer-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
-            echo "$SERVICE_NAME started ..."
+            echo "$SERVICE_NAME started..."
         fi
     ;;
     stop)
         if [ -f ${PID_PATH_NAME} ]; then
             PID=$(cat ${PID_PATH_NAME});
-            echo "$SERVICE_NAME stopping ..."
+            echo "$SERVICE_NAME stopping..."
             kill ${PID};
-            echo "$SERVICE_NAME stopped ..."
+            echo "$SERVICE_NAME stopped..."
             rm ${PID_PATH_NAME}
         else
-            echo "$SERVICE_NAME is not running ..."
+            echo "$SERVICE_NAME is not running..."
         fi
     ;;
     restart)
         if [ -f ${PID_PATH_NAME} ]; then
             PID=$(cat ${PID_PATH_NAME});
-            echo "$SERVICE_NAME stopping ...";
+            echo "$SERVICE_NAME stopping...";
             kill ${PID};
-            echo "$SERVICE_NAME stopped ...";
+            echo "$SERVICE_NAME stopped...";
             rm ${PID_PATH_NAME}
-            echo "$SERVICE_NAME starting ..."
+            echo "$SERVICE_NAME starting..."
             nohup java -jar /app/time-is-money-backend/target/timer-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
-            echo "$SERVICE_NAME started ..."
+            echo "$SERVICE_NAME started..."
         else
-            echo "$SERVICE_NAME is not running ..."
+            echo "$SERVICE_NAME is not running..."
         fi
     ;;
 esac
